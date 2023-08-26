45cce4c0bff838bd6b57d74dc49079a2da9c74cc
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-08-26 04:56:09 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# creates a file
 file { '/tmp/school':
   ensure  => file,
-  content => 'I love Puppet',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
