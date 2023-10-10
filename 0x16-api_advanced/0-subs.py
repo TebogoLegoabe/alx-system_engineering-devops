d2fb395293c3c959b46ad7bd196a30b83c9bce7d
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-10-10 21:29:36 +0200

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-#Get subs
+# get subs
 from requests import get
 from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number 
-    of subscribers for a given subreddit"""
+    """subs"""
     head = {'User-Agent': 'Dan Kazam'}
     count = get('https://www.reddit.com/r/{}/about.json'.format(
         subreddit), headers=head).json()
