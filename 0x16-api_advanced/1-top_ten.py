00e70eea1dbaf9294620f16a9dd3a43ac1e6043f
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-10-10 21:49:34 +0200

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """Query Reddit API to determine subreddit sub count"""
-from requests import get
-from sys import argv
+import requests
 
 
 def top_ten(subreddit):
-     """Request top ten hot posts of subreddit from Reddit API"""
-    head = {'User-Agent': 'Dan Kazam'}
-    try:
-        count = get('https://www.reddit.com/r/{}/hot.json?count=10'.format(
-            subreddit), headers=head).json().get('data').get('children')
-        print('\n'.join([dic.get('data').get('title')
-                         for dic in count][:10]))
-    except:
-        print('None')
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    """prints titles of the first 10hot posts"""
+    headers = {
+        "User-Agent": "0x16. API_advanced-e_kiminza"
+    }
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return
+    response_ = response.json().get("data")
+    for child in response_.get("children"):
+        print(child.get("data").get("title"))
